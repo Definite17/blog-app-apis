be6288dd2822e8c79227eb4277d1bf4050230e4c
Ashraf <ashrafnawaz17@gmail.com>
2022-10-07 00:02:49 +0530

    fixes

--- src/main/java/com/ashraf/blog/entities/User.java
+++ src/main/java/com/ashraf/blog/entities/User.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class User {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column(name = "user_name", nullable = false, length = 100)
