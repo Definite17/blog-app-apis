e7e6637526bc3982f643f24b81e5bd8577b4e598
Ashraf <ashrafnawaz17@gmail.com>
2022-10-24 21:59:34 +0530

    fixes

--- src/main/java/com/ashraf/blog/config/SecurityConfig.java
+++ src/main/java/com/ashraf/blog/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
