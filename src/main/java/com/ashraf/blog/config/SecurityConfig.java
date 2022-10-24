619538737d7521e9cca5d2b9be64114a17b00fef
Ashraf <ashrafnawaz17@gmail.com>
2022-10-24 12:53:46 +0530

    Password Authentication and encoding

--- src/main/java/com/ashraf/blog/config/SecurityConfig.java
+++ src/main/java/com/ashraf/blog/config/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.ashraf.blog.config;
 
+import com.ashraf.blog.Security.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class SecurityConfig{
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,9 +29,19 @@ public class SecurityConfig{
         return http.build();
     }
 
-//    @Bean
-//    public Authe
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http)throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(this.customUserDetailService)
+                .passwordEncoder(bCryptPasswordEncoder())
+                .and()
+                .build();
+    }
 
-//    @Bean
-//    public PasswordEncoder b
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+
+    }
 }
